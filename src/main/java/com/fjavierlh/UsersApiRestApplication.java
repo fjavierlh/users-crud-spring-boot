0c003fe4955937309f14ac4d0e4a8a692cfbd938
fjavierlh <fjavierlh@gmail.com>
2021-04-11 17:30:35 +0200

    Init commit

---
+++ src/main/java/com/fjavierlh/UsersApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.fjavierlh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fjavierlh/UsersApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjavierlh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
