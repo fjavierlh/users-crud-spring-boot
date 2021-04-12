5f62dbbf7481f02a395bc18214958f11b4ea778b
fjavierlh <fjavierlh@gmail.com>
2021-04-12 15:00:24 +0200

    Added insert and save methods

--- src/main/java/com/fjavierlh/web/UserController.java
+++ src/main/java/com/fjavierlh/web/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.fjavierlh.dao.IUserDao;
 import com.fjavierlh.domain.User;
@@ -22,6 +23,20 @@ public class UserController {
 		model.addAttribute("users", users);
 		
 		return "index";
+	}
+	/*
+	@GetMapping("/insert")
+	public String insert(User user) {
+		
+		return "update";
+	}
+	*/
+	@PostMapping("/save")
+	public String save(User user) {
+		
+		this.userService.saveUser(user);
+		
+		return "redirect:/";
 	}
 
 }
